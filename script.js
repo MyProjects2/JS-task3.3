5c7ba7880c09fc61c49ff652ed486172e7ab2bb2
Olga <olgayakimovich87@gmail.com>
2018-02-14 09:26:58 +0300

    a function without using cycles

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 'use strict'
 var userInput = prompt('Напишите любое слово');
-function calculateVowels(string) {
-    var vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'э', 'ю', 'я', 'А', 'Е', 'Ё', 'И', 'О', 'У', 'Э', 'Ю', 'Я'];
-    var array = [];
-    for (var i = 0; i <= vowels.length; i++){
-        var position = 0;
-        while (true) {
-            var foundPos = string.indexOf(vowels[i], position);
-            if (foundPos == -1) break;
-            array.push(foundPos);
-            position = foundPos + 1; 
-        }
-    }
-        return array.length;  
-}
-console.log('Количество гласных букв в слове: ' + calculateVowels(userInput));
+const countVowels = str => Array.from(str).filter(letter => 'aeёиоуэюяАЕЁИОУЭЮЯ'.includes(letter)).length;
+console.log(`Количество гласных букв в слове: ${countVowels(userInput)}`);
+
 
 
 
